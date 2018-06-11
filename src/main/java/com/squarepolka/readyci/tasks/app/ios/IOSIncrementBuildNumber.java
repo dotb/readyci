2c0f31b44ea4470e5a97ae1670928baf1a4c9dfd
Bradley Clayton <bclayton@deloitte.com.au>
2018-06-11 20:27:57 +0200

    Added a increment build number task for iOS

---
+++ src/main/java/com/squarepolka/readyci/tasks/app/ios/IOSIncrementBuildNumber.java
@@ -0,0 +1,32 @@
+package com.squarepolka.readyci.tasks.app.ios;
+
+import com.dd.plist.NSDictionary;
+import com.dd.plist.PropertyListParser;
+import com.squarepolka.readyci.taskrunner.BuildEnvironment;
+import com.squarepolka.readyci.tasks.Task;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class IOSIncrementBuildNumber extends Task {
+    @Override
+    public String taskIdentifier() {
+        return "ios_increment_build_number";
+    }
+
+    @Override
+    public void performTask(BuildEnvironment buildEnvironment) throws Exception {
+        String workspaceName = buildEnvironment.buildParameters.get("workspace");
+        String infoPlistPath = String.format("%s/%s/Resources/Info.plist", buildEnvironment.buildPath, workspaceName);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyMMddhhmm");
+        String formattedDate = simpleDateFormat.format(new Date());
+
+        File infoPlistFile = new File(infoPlistPath);
+        NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(infoPlistFile);
+        rootDict.put("CFBundleVersion", formattedDate);
+        PropertyListParser.saveAsXML(rootDict, infoPlistFile);
+    }
+}
