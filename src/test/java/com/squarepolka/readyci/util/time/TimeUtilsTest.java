7f79b2904e20ff0e576c3fb5c6331ea3759a41b8
Bradley Clayton <bclayton@deloitte.com.au>
2018-08-03 02:58:05 +0200

    Fixed TimeUtils tests.

--- src/test/java/com/squarepolka/readyci/util/time/TimeUtilsTest.java
+++ src/test/java/com/squarepolka/readyci/util/time/TimeUtilsTest.java
@@ -13,17 +13,17 @@ public class TimeUtilsTest {
     }
 
     @Test
-    void getFormattedTaskTime(long timeDifference) {
+    void getFormattedTaskTime() {
         TimeUtils subject = new TimeUtils();
         String result = subject.getFormattedTaskTime(2000);
         assertEquals("0 days 0 hours 0 minutes 2 seconds 0 milliseconds", result, "Formatted time is correct");
     }
 
     @Test
-    void convertMillisecondsToReadableTime(long totalTime, long timeRemaining, long timeUnit) {
+    void convertMillisecondsToReadableTime() {
         TimeUtils subject = new TimeUtils();
         TimeUnit result = subject.convertMillisecondsToReadableTime(2000, 1000, TimeUtils.oneSecond);
 
-        assertEquals(result.calculatedTime, 1000, "Calculated time for seconds is correct");
+        assertEquals(1, result.calculatedTime, "Calculated time for seconds is correct");
     }
 }
