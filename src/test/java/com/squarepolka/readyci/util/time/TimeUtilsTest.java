99679435ce7c84f7dbc262ddf40a33a62d0b85af
Bradley Clayton <bclayton@deloitte.com.au>
2018-08-03 08:18:11 +0200

    Cleaned up tests and fixed running tests with maven

--- src/test/java/com/squarepolka/readyci/util/time/TimeUtilsTest.java
+++ src/test/java/com/squarepolka/readyci/util/time/TimeUtilsTest.java
@@ -1,29 +1,28 @@
 package com.squarepolka.readyci.util.time;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeUtilsTest {
 
-    @BeforeEach
-    void setUp() {
+    public TimeUtils subject;
 
+    @Before
+    public void setUp() {
+        subject = new TimeUtils();
     }
 
     @Test
-    void getFormattedTaskTime() {
-        TimeUtils subject = new TimeUtils();
+    public void getFormattedTaskTime() {
         String result = subject.getFormattedTaskTime(2000);
         assertEquals("0 days 0 hours 0 minutes 2 seconds 0 milliseconds", result, "Formatted time is correct");
     }
 
     @Test
-    void convertMillisecondsToReadableTime() {
-        TimeUtils subject = new TimeUtils();
+    public void convertMillisecondsToReadableTime() {
         TimeUnit result = subject.convertMillisecondsToReadableTime(2000, 1000, TimeUtils.oneSecond);
-
         assertEquals(1, result.calculatedTime, "Calculated time for seconds is correct");
     }
 }
