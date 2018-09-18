7a123869a3808f4e3b2c26921857235d9effd494
David Tao <davtao@deloitte.com.au>
2018-09-18 14:08:16 +1000

    Removed redundant imports

--- src/main/java/com/squarepolka/readyci/tasks/app/android/AndroidVersionIncrement.java
+++ src/main/java/com/squarepolka/readyci/tasks/app/android/AndroidVersionIncrement.java
@@ -2,19 +2,15 @@ package com.squarepolka.readyci.tasks.app.android;
 
 import com.squarepolka.readyci.taskrunner.BuildEnvironment;
 import com.squarepolka.readyci.tasks.Task;
-import com.squarepolka.readyci.tasks.code.GitCheckout;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 @Component
 public class AndroidVersionIncrement extends Task {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GitCheckout.class);
 
     static final String TASK_INCREMENT_VERSION = "android_task_increment";
     static final String VERSION_PROP_FILE = "version.properties";
