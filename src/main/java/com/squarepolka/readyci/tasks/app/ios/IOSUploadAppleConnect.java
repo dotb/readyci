a91c667abea5cc537b0ed2c29ea163c4dcb8a9f5
Bradley Clayton <bclayton@deloitte.com.au>
2019-01-30 21:49:53 +1100

    [FIX] Updated the iOS Apple Connect tasks to be aware of the path set by xcode-select so that multiple installed versions of xcode is supported.

--- src/main/java/com/squarepolka/readyci/tasks/app/ios/IOSUploadAppleConnect.java
+++ src/main/java/com/squarepolka/readyci/tasks/app/ios/IOSUploadAppleConnect.java
@@ -4,10 +4,15 @@ import com.squarepolka.readyci.taskrunner.BuildEnvironment;
 import com.squarepolka.readyci.tasks.Task;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
+
 @Component
 public class IOSUploadAppleConnect extends Task {
 
     public static final String TASK_UPLOAD_ITUNES_CONNECT = "ios_upload_itunes_connect";
+    private static final String COMMAND_XCODE_SELECT = "xcode-select";
+    private static final String PARAM_XCODE_SELECT_PATH = "--print-path";
+    private static final String PATH_XCODE_DEFAULT = "/Applications/Xcode.app/Contents";
 
     @Override
     public String taskIdentifier() {
@@ -21,11 +26,23 @@ public class IOSUploadAppleConnect extends Task {
         String exportPath = String.format("%s/%s.ipa", buildEnvironment.scratchPath, scheme);
         String iTunesUsername = buildEnvironment.getProperty("iTunesUsername");
         String iTunesPassword = buildEnvironment.getProperty("iTunesPassword");
+        String xCodePath = getXCodeSelectPath(buildEnvironment);
+        String altoolPath = xCodePath + "/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool";
 
-        executeCommand(new String[] {"/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool",
+        executeCommand(new String[] {altoolPath,
         "--upload-app",
         "-f", exportPath,
         "-u", iTunesUsername,
         "-p", iTunesPassword});
+    }
+
+    protected String getXCodeSelectPath(BuildEnvironment buildEnvironment) {
+        InputStream inputStream = executeCommand(new String[]{COMMAND_XCODE_SELECT, PARAM_XCODE_SELECT_PATH});
+        java.util.Scanner scanner = new java.util.Scanner(inputStream).useDelimiter("\\A");
+        String xCodeSelectPath = scanner.hasNext() ? scanner.next() : PATH_XCODE_DEFAULT;
+        xCodeSelectPath = xCodeSelectPath.replace("\n", "");
+        xCodeSelectPath = xCodeSelectPath.replace("\r", "");
+        xCodeSelectPath = xCodeSelectPath.replace("/Developer", "");
+        return xCodeSelectPath;
     }
 }
